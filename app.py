821fd6623c44e0c0baca69defbf4a81772840de3
Gruhan Ancha <gruhanancha@gmail.com>
2024-10-05 12:26:14 +0530

    Your commit message here

--- app.py
+++ app.py
@@ -1,53 +1,51 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, jsonify
 import numpy as np
-import pandas
-import sklearn
 import pickle
 
 # importing model
-model = pickle.load(open('model.pkl','rb'))
-sc = pickle.load(open('standscaler.pkl','rb'))
-ms = pickle.load(open('minmaxscaler.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+sc = pickle.load(open('standscaler.pkl', 'rb'))
+ms = pickle.load(open('minmaxscaler.pkl', 'rb'))
 
 # creating flask app
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
-
-@app.route("/predict",methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    N = request.form['Nitrogen']
-    P = request.form['Phosporus']
-    K = request.form['Potassium']
-    temp = request.form['Temperature']
-    humidity = request.form['Humidity']
-    ph = request.form['Ph']
-    rainfall = request.form['Rainfall']
+    try:
+        data = request.json
+        N = data['Nitrogen']
+        P = data['Phosporus']
+        K = data['Potassium']
+        temp = data['Temperature']
+        humidity = data['Humidity']
+        ph = data['Ph']
+        rainfall = data['Rainfall']
 
-    feature_list = [N, P, K, temp, humidity, ph, rainfall]
-    single_pred = np.array(feature_list).reshape(1, -1)
+        feature_list = [N, P, K, temp, humidity, ph, rainfall]
+        single_pred = np.array(feature_list).reshape(1, -1)
 
-    scaled_features = ms.transform(single_pred)
-    final_features = sc.transform(scaled_features)
-    prediction = model.predict(final_features)
+        scaled_features = ms.transform(single_pred)
+        final_features = sc.transform(scaled_features)
+        prediction = model.predict(final_features)
 
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+        crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                     8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                     14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                     19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
 
-    if prediction[0] in crop_dict:
-        crop = crop_dict[prediction[0]]
-        result = "{} is the best crop to be cultivated right there".format(crop)
-    else:
-        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
-    return render_template('index.html',result = result)
+        if prediction[0] in crop_dict:
+            crop = crop_dict[prediction[0]]
+            result = f"{crop} is the best crop to be cultivated right there"
+        else:
+            result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
 
+        return jsonify({"success": True, "result": result})
 
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 400
 
-
-# python main
-if __name__ == "__main__":
-    app.run(debug=True)
+# Add a simple route for the root URL
+@app.route('/')
+def home():
+    return "Crop Prediction API is running!"
